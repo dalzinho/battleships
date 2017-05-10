70298c0568b1e87c5eef3380e805f6330d0e7271
John Daly <jpdaly@gmail.com>
2017-05-10 22:06:08 +0100

    initial commit, create files and boilerplate for classes and unit tests

---
+++ src/battleships/ConsoleTest.java
@@ -0,0 +1,19 @@
+package battleships;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConsoleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/battleships/TrackingBoardTest.java
@@ -0,0 +1,19 @@
+package battleships;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrackingBoardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
